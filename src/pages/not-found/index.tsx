3fbd42312c75c74f19ea02bb56ee03d5b45c6528
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-06-23 14:15:13 +0400

    feat: add not found page

---
+++ src/pages/not-found/index.tsx
@@ -0,0 +1,9 @@
+import { HomeButton } from "@/components/home-button";
+import { Title } from "@/components/typography";
+
+export const NotFoundPage = () => (
+    <>
+        <Title level={1}>Page not found</Title>
+        <HomeButton />
+    </>
+);
